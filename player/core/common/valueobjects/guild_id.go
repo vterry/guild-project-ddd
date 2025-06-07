e94516a28ed0c2c233b0afd170545ee7cf0fb589
Victor Souza <victor.souza2@gmail.com>
2025-06-07 00:26:37 -0300

    clean up player module

--- player/core/common/valueobjects/guild_id.go
+++
@@ -1,9 +0,0 @@
-package valueobjects
-
-type GuildID struct {
-	BaseID[string]
-}
-
-func (pID GuildID) Equals(otherID GuildID) bool {
-	return pID.BaseID.Equals(otherID.BaseID)
-}
--- player/core/common/valueobjects/item_id.go
+++
@@ -1,9 +0,0 @@
-package valueobjects
-
-type ItemID struct {
-	BaseID[string]
-}
-
-func (i ItemID) Equals(otherID ItemID) bool {
-	return i.BaseID.Equals(otherID.BaseID)
-}
